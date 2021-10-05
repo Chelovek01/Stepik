cd4b88f65bebb78fa0f66af313e5e3a056a709e7
Chelovek <ntl5551@mail.ru>
2021-10-05 13:22:10 +0300

    solution

---  Generation Python: Advanced Course/2.1/Number flip.py
+++  Generation Python: Advanced Course/2.1/Number flip.py
@@ -1,12 +1,2 @@
 number = input()
-list1 = []
-list2 = list1[-5:]
-list3 = list2.reverse()
-print(list3)
-for i in number:
-    list1 += i
-print(list1)
-if len(list1) > 5:
-    print(''.join(list1[0:1]), ''.join(list2.reverse()))
-else:
-    False
+print(int(number[:-5] + number[-5:][::-1]))
