283196ccb73e25e54fc2870be0d3b011f6d49585
Chelovek <ntl5551@mail.ru>
2021-10-29 19:11:31 +0300

    solution

---
+++ Python: Basics and Applications/3.2/Step_7.py
@@ -0,0 +1,9 @@
+import re, sys
+
+pattern = r'cat'
+
+for line in sys.stdin:
+    line = line.rstrip()
+    all_inclusions = re.findall(pattern, line)
+    if len(all_inclusions) >= 2:
+        print(line)
